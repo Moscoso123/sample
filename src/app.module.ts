fb5b99bf3bf30dda513e44e49f8c035c2a9205a4
Moscoso1 <parkwilfrem@gmail.com>
2026-02-11 06:47:14 +0800

    new

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+// app.module.ts - FINAL VERSION
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
@@ -8,9 +9,18 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    // TEMPORARILY REMOVE TypeOrmModule to test app starts
-    // TypeOrmModule.forRoot({ ... }),
-    
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      // Use the NEW user you created
+      host: 'mysql.railway.internal',
+      port: 3306,
+      username: 'app_user', // ← The user YOU created
+      password: 'StrongPass123!', // ← The password YOU set
+      database: 'railway',
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production', // false in production
+      ssl: false, // Internal doesn't need SSL
+    }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       serveRoot: '/',
