d22195b6aef65ca822aa9bf829c9242b96c68a40
Moscoso1 <parkwilfrem@gmail.com>
2026-02-11 06:55:03 +0800

    sdfsdf

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,8 @@ import { UsersModule } from './users/users.module';
       // Use the NEW user you created
       host: 'mysql.railway.internal',
       port: 3306,
-      username: 'app_user', // ← The user YOU created
-      password: 'StrongPass123!', // ← The password YOU set
+      username: 'root', // ← The user YOU created
+      password: 'XcPxQFeTIPHpFRsrCwgiuinpZpChiFfh', // ← The password YOU set
       database: 'railway',
       autoLoadEntities: true,
       synchronize: process.env.NODE_ENV !== 'production', // false in production
